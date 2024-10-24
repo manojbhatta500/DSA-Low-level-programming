fd758ed9313dca794006ac849e88188b28a620ea
manojbhatta500 <manojbhatta500@gmail.com>
2024-10-24 11:29:48 +0545

    added solution 1

--- solutions/solution_1.cpp
+++ solutions/solution_1.cpp
@@ -1,1 +1,20 @@
-// solution cpp 
+#include<iostream>
+
+using namespace std;
+
+ int main () {
+  int n;
+ cout<< "please enter the number n : ";
+  cin >> n;
+  int i = 0;
+   while(i < n){
+    int j = 0;
+    while (j < n) {
+      std::cout << "*";
+      j++;
+    }
+    cout<< endl;
+    i++;
+  }
+}
+//this is working 
